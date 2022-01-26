459c7138f58e07fd03d691ce4bef2257ef5a11b3
Jinan <jinanjeong@snu.ac.kr>
2022-01-27 02:18:05 +0900

    셋팅 거의 끝남

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'utils/firebase_options.dart';
 
-void main() {
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(
     MaterialApp(
       home: MyApp()
